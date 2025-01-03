04f23541bca81ea9b192b4c0b64c8951bb1a70b4
unknown <a.cina@procreatech.com>
2025-01-03 20:46:10 +0100

    22. Setting Up the Course Component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,27 +12,5 @@ import { createHttpObservable } from "../common/util";
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    // const interval1$ = interval(1000);
-    // const interval2$ = interval1$.pipe(map((val) => val * 10));
-    // const result$ = merge(interval1$, interval2$);
-    // result$.subscribe(console.log);
-
-    // definiamo un observable interval e mostriamo un valore ogni secondo
-    const interval1$ = interval(1000);
-
-    // non vogliamo più ricevere valori dopo 5 secondi
-    // dobbiamo creare una reference alla subscription
-    const sub = interval1$.subscribe(console.log);
-
-    // e chiamare il metodo unsubscribe() della reference sub
-    setTimeout(() => sub.unsubscribe(), 5000);
-
-    const courses$ = createHttpObservable("/api/courses");
-
-    const subscriptionCourses = courses$.subscribe(console.log);
-
-    // effettuo l'unsubscribe con delay 0, avrò in console una chiamata cancellata
-    setTimeout(() => subscriptionCourses.unsubscribe(), 0);
-  }
+  ngOnInit() {}
 }
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,58 +1,59 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Course} from "../model/course";
 import {
-    debounceTime,
-    distinctUntilChanged,
-    startWith,
-    tap,
-    delay,
-    map,
-    concatMap,
-    switchMap,
-    withLatestFrom,
-    concatAll, shareReplay
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  map,
+  concatMap,
+  switchMap,
+  withLatestFrom,
+  concatAll,
+  shareReplay,
+} from "rxjs/operators";
+import { merge, fromEvent, Observable, concat } from "rxjs";
+import { Lesson } from "../model/lesson";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css'],
-    standalone: false
+  selector: "course",
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.css"],
+  standalone: false,
 })
 export class CourseComponent implements OnInit, AfterViewInit {
+  course$: Observable<Course>;
+  lessons$: Observable<Lesson[]>;
 
+  @ViewChild("searchInput", { static: true }) input: ElementRef;
 
-    course$: Observable<Course>;
-    lessons$: Observable<Lesson[]>;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    const courseId = this.route.snapshot.params["id"];
 
-    @ViewChild('searchInput', { static: true }) input: ElementRef;
+    // definiamo l'observable course$ andando ad effettuare una call http passando l'id del corso
+    // il course$ viene sottoscritto direttamente nel template tramite pipe async
+    this.course$ = createHttpObservable(`/api/courses/${courseId}`);
 
-    constructor(private route: ActivatedRoute) {
+    // definiamo l'observable lessons$ che sarà un array di lezioni che fanno parte del corso che mostriamo
+    // è una lista iniziale che poi potremo filtrare tramite Search Typeahead
+    this.lessons$ = createHttpObservable(
+      `/api/lessons?courseId=${courseId}&pageSize=100`
+    ).pipe(
+      tap(console.log),
+      map((response) => response["payload"])
+    );
+  }
 
-
-    }
-
-    ngOnInit() {
-
-        const courseId = this.route.snapshot.params['id'];
-
-
-
-    }
-
-    ngAfterViewInit() {
-
-
-
-
-    }
-
-
-
-
+  ngAfterViewInit() {}
 }
